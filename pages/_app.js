8b38c7397db9bf2bd7f7a5fe7339b6056d8030cf
Antonio Ruiz <16592002+AntonioRuizNet@users.noreply.github.com>
2025-12-09 17:18:32 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,12 +4,28 @@ import { Provider } from "react-redux";
 import { store } from "@/store";
 import { brand } from "@/styles/fonts";
 import Head from "next/head";
+import Script from "next/script";
 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <Provider store={store}>
       <Head>
         <link rel="manifest" href="/manifest.json" />
+
+        {/* Google Analytics */}
+        <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-J0M0G5CRTG" />
+
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-J0M0G5CRTG', {
+            page_path: window.location.pathname,
+          });
+        `}
+        </Script>
+
         <meta name="theme-color" content="#46a28d" />
 
         {/* Favicon por compatibilidad */}
